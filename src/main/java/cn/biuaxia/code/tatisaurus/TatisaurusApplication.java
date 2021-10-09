260dc5bee00402fbaf11ee03ca116fd938e5890f
biuaxia <biuaxia@qq.com>
2021-10-09 16:08:30 +0800

    chore: project initialization

---
+++ src/main/java/cn/biuaxia/code/tatisaurus/TatisaurusApplication.java
@@ -0,0 +1,13 @@
+package cn.biuaxia.code.tatisaurus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TatisaurusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TatisaurusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/biuaxia/code/tatisaurus/TatisaurusApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.biuaxia.code.tatisaurus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TatisaurusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
